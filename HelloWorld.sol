a1dfc96d5791c1171bdc7e15a55ce5665720408a
alex-njoya <njoyaalexander71@gmail.com>
2024-08-02 08:39:27 +0000

    solidityByExample

---
+++ HelloWorld.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.0 and less than 0.9.0
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string public greet = "Hello World!";
+
+
+}
